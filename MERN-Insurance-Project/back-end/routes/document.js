0e654b7585f1d9c584447b72c05decc4e9932139
projectsbyMW <madheshwaran63@gmail.com>
2024-10-20 04:37:31 +0530

    Update document.js

--- MERN-Insurance-Project/back-end/routes/document.js
+++ MERN-Insurance-Project/back-end/routes/document.js
@@ -7,7 +7,9 @@ const { protect } = require('../middleware/authmiddleware');
 
 // Initialize S3 Client
 const s3Client = new S3Client({
+    region: process.env.AWS_REGION,
     credentials: {
+
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     },
